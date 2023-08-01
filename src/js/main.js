de9cd00a41035a4368b92e862ccdb660c9cba115
sameckmatheus <sameckmatheuspro@gmail.com>
2023-07-31 21:21:00 -0300

    Update: Displacement Slider

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,37 @@ const SliderControl =  function (opts) {
         }
     `;
 
+    let fragment = `
+        
+        varying vec2 vUv;
+
+        uniform sampler2D currentImage;
+        uniform sampler2D nextImage;
+
+        uniform float dispFactor;
+
+        void main() {
+
+            vec2 uv = vUv;
+            vec4 _currentImage;
+            vec4 _nextImage;
+            float intensity = 0.3;
+
+            vec4 orig1 = texture2D(currentImage, uv);
+            vec4 orig2 = texture2D(nextImage, uv);
+            
+            _currentImage = texture2D(currentImage, vec2(uv.x, uv.y + dispFactor * (orig2 * intensity)));
+
+            _nextImage = texture2D(nextImage, vec2(uv.x, uv.y + (1.0 - dispFactor) * (orig1 * intensity)));
+
+            vec4 finalTexture = mix(_currentImage, _nextImage, dispFactor);
+
+            gl_FragColor = finalTexture;
+
+        }
+    `;
+
+    
 };
 
 initializeSlider (document.querySelectorAll('img'), () => {

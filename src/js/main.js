ad1d94afc618c72e8844e4ae2fa0559ea4590bbd
sameckmatheus <sameckmatheuspro@gmail.com>
2023-07-31 21:18:39 -0300

    Update: Change Displacement Slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,13 @@
+const SliderControl =  function (opts) {
+    let vertex = `
+        varying vec2 vUv;
+        void main() {
+            vUv = uv;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+        }
+    `;
 
+};
 
 initializeSlider (document.querySelectorAll('img'), () => {
     document.body.classList.remove('loading');

54cbe96eb948132c1dcd2af884915d6335046d31
sameckmatheus <sameckmatheuspro@gmail.com>
2023-08-01 15:19:14 -0300

    Update: All Project

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,20 @@
-const SliderControl =  function (opts) {
+const displacementSlider = function(opts) {
+
     let vertex = `
         varying vec2 vUv;
         void main() {
-            vUv = uv;
-            gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+          vUv = uv;
+          gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
         }
     `;
 
     let fragment = `
         varying vec2 vUv;
-
         uniform sampler2D currentImage;
         uniform sampler2D nextImage;
-
         uniform float dispFactor;
 
         void main() {
-
             vec2 uv = vUv;
             vec4 _currentImage;
             vec4 _nextImage;
@@ -44,7 +42,7 @@ const SliderControl =  function (opts) {
 
     let renderW, renderH;
 
-    if (renderWidth < canvasWidth) {
+    if( renderWidth > canvasWidth ) {
         renderW = renderWidth;
     } else {
         renderW = canvasWidth;
@@ -60,8 +58,127 @@ const SliderControl =  function (opts) {
     renderer.setClearColor( 0x23272A, 1.0 );
     renderer.setSize( renderW, renderH );
     parent.appendChild( renderer.domElement );
+    
+    let loader = new THREE.TextureLoader();
+    loader.crossOrigin = "anonymous";
 
+    images.forEach( ( img ) => {
+        image = loader.load( img.getAttribute( 'src' ) + '?v=' + Date.now() );
+        image.magFilter = image.minFilter = THREE.LinearFilter;
+        image.anisotropy = renderer.capabilities.getMaxAnisotropy();
+        sliderImages.push( image );
+    });
 
+    let scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0x23272A );
+
+    let camera = new THREE.OrthographicCamera(
+        renderWidth / -2,
+        renderWidth / 2,
+        renderHeight / 2,
+        renderHeight / -2,
+        1,
+        1000
+    );
+
+    camera.position.z = 1;
+
+    let mat = new THREE.ShaderMaterial({
+        uniforms: {
+            dispFactor: { type: "f", value: 0.0 },
+            currentImage: { type: "t", value: sliderImages[0] },
+            nextImage: { type: "t", value: sliderImages[1] },
+        },
+        vertexShader: vertex,
+        fragmentShader: fragment,
+        transparent: true,
+        opacity: 1.0
+    });
+
+    let geometry = new THREE.PlaneBufferGeometry(
+        parent.offsetWidth,
+        parent.offsetHeight,
+        1
+    );
+
+    let object = new THREE.Mesh(geometry, mat);
+    object.position.set(0, 0, 0);
+    scene.add(object);
+
+    let addEvents = function(){
+
+        let pagButtons = Array.from(document.getElementById('pagination').querySelectorAll('button'));
+        let isAnimating = false;
+
+        pagButtons.forEach( (el) => {
+            el.addEventListener('click', function() {
+                if( !isAnimating ) {
+                    isAnimating = true;
+                    document.getElementById('pagination').querySelectorAll('.active')[0].className = '';
+                    this.className = 'active';
+
+                    let slideId = parseInt( this.dataset.slide, 10 );
+
+                    mat.uniforms.nextImage.value = sliderImages[slideId];
+                    mat.uniforms.nextImage.needsUpdate = true;
+
+                    TweenLite.to( mat.uniforms.dispFactor, 1, {
+                        value: 1,
+                        ease: 'Expo.easeInOut',
+                        onComplete: function () {
+                            mat.uniforms.currentImage.value = sliderImages[slideId];
+                            mat.uniforms.currentImage.needsUpdate = true;
+                            mat.uniforms.dispFactor.value = 0.0;
+                            isAnimating = false;
+                        }
+                    });
+
+                    let slideTitleEl = document.getElementById('slide-title');
+                    let slideStatusEl = document.getElementById('slide-status');
+                    let nextSlideTitle = document.querySelectorAll(`[data-slide-title="${slideId}"]`)[0].innerHTML;
+                    let nextSlideStatus = document.querySelectorAll(`[data-slide-status="${slideId}"]`)[0].innerHTML;
+
+                    TweenLite.fromTo( slideTitleEl, 0.5,
+                        {
+                            autoAlpha: 1,
+                            y: 0
+                        },
+                        {
+                            autoAlpha: 0,
+                            y: 20,
+                            ease: 'Expo.easeIn',
+                            onComplete: function () {
+                                slideTitleEl.innerHTML = nextSlideTitle;
+                                TweenLite.to( slideTitleEl, 0.5, {
+                                    autoAlpha: 1,
+                                    y: 0,
+                                })
+                            }
+                        });
+
+                    TweenLite.fromTo( slideStatusEl, 0.5,
+                        {
+                            autoAlpha: 1,
+                            y: 0
+                        },
+                        {
+                            autoAlpha: 0,
+                            y: 20,
+                            ease: 'Expo.easeIn',
+                            onComplete: function () {
+                                slideStatusEl.innerHTML = nextSlideStatus;
+                                TweenLite.to( slideStatusEl, 0.5, {
+                                    autoAlpha: 1,
+                                    y: 0,
+                                    delay: 0.1,
+                                })
+                            }
+                        }
+                    );
+                };
+            });
+        });
+    };
 
     addEvents();
 
@@ -71,19 +188,18 @@ const SliderControl =  function (opts) {
 
     let animate = function() {
         requestAnimationFrame(animate);
-
         renderer.render(scene, camera);
     };
     animate();
 };
 
-initializeSlider (document.querySelectorAll('img'), () => {
+imagesLoaded( document.querySelectorAll('img'), () => {
     document.body.classList.remove('loading');
 
     const el = document.getElementById('slider');
     const imgs = Array.from(el.querySelectorAll('img'));
-    new DisplamentSlider ({
-        parant: el,
+    new displacementSlider({
+        parent: el,
         images: imgs
-    })
+    });
 });
